d7bdbc68e43ea9d1e909da814fc76f8f688f2011
guashasha <davidcarrionr00@hotmail.com>
2024-05-17 00:21:08 -0600

    creado DTO property

--- codigoInmobiliaria/src/main/kotlin/DTO/Property.kt
+++ codigoInmobiliaria/src/main/kotlin/DTO/Property.kt
@@ -1,5 +1,20 @@
 package DTO
-@Setter
-class Property {
 
-}
+enum class PropertyState {AVAILABLE, OCCUPIED, SUSPENDED}
+
+enum class PropertyType {ALL, BUILDING, HOUSE, APARTMENT, PREMISES}
+
+enum class PropertyAction {SELL, RENT}
+
+data class Property (
+  val id: Int,
+  var title: String,
+  var shortDescription: String,
+  var fullDescription: String,
+  val type: PropertyType,
+  var price: Float,
+  var state: PropertyState,
+  val direction: String,
+  val houseOwner: Int,
+  var action: PropertyAction
+)
