18c5d8b4459f1adf0de3cb07d8b9021e8ec5f855
Pale04 <epalemolina@gmail.com>
2024-05-23 13:20:13 -0600

    Caso de uso registrar visita

--- inmobiliaria/src/main/kotlin/DAO/VisitDAO.kt
+++ inmobiliaria/src/main/kotlin/DAO/VisitDAO.kt
@@ -23,35 +23,42 @@ class VisitDAO {
     val dbConnection = DataBaseConnection().connection
 
     fun getUnavailableVisits (idProperty : UInt, date : Date) : VisitResult {
-        if (idProperty == 0U) {
-            return VisitResult.Failure()
-        }
+        if (idProperty == 0U) return VisitResult.Failure()
 
         val unavailableVisitsList = ArrayList<Visit>()
 
         return try {
             val query = dbConnection.prepareStatement("SELECT * FROM visit WHERE propertyid = ? AND DATE = ?")
-
             query.setInt(1,idProperty.toInt())
             query.setDate(2,date)
 
             var result = query.executeQuery()
 
-            while (result.next()) {
-                unavailableVisitsList.add(Visit.fromResultSet(result))
-            }
+            while (result.next()) unavailableVisitsList.add(Visit.fromResultSet(result))
 
             VisitResult.FoundList(unavailableVisitsList)
         }
         catch (error: SQLException) {
             VisitResult.DBError(error.message.toString())
         }
-
     }
 
-    /*fun add (visit: Visit): VisitResult {
+    fun add (visit: Visit): VisitResult {
+        if (!visit.isValidForAdd()) return VisitResult.Failure()
 
-    }*/
+        return try {
+            val query = dbConnection.prepareStatement("INSERT INTO visit (clientId,propertyId,DATE,TIME) VALUES (?,?,?,?)")
+            query.setInt(1,visit.clientId.toInt())
+            query.setInt(2,visit.propertyId.toInt())
+            query.setDate(3,visit.date)
+            query.setTime(4,visit.time)
+
+            if (query.executeUpdate() > 0) VisitResult.Success() else VisitResult.WrongVisit()
+        }
+        catch (error: SQLException) {
+            VisitResult.DBError(error.message.toString())
+        }
+    }
     // fun modify (visit: Visit): VisitResult {}
     // fun getById (visitId: UInt): VisitResult {}
     // fun getAll (): VisitResult {}
--- inmobiliaria/src/main/kotlin/GUI/ScheduleVisitController.kt
+++ inmobiliaria/src/main/kotlin/GUI/ScheduleVisitController.kt
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.control.Alert
+import javafx.scene.control.Alert.AlertType
 import javafx.scene.control.DatePicker
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.GridPane
@@ -24,7 +25,8 @@ fun main (args: Array<String>) {
 class ScheduleVisitController : Application() {
     @FXML lateinit var gpSchedule : GridPane
     @FXML lateinit var dpDate : DatePicker
-    var idProperty : UInt = 1U
+    var propertyId : UInt = 1U
+    var clientId : UInt = 1U
     override fun start (primaryStage: Stage) {
         try {
             val parent: Parent? = FXMLLoader.load<Parent>(javaClass.getResource("/FXML/ScheduleVisit.fxml"))
@@ -44,58 +46,62 @@ class ScheduleVisitController : Application() {
 
     }
 
-    @FXML fun buscarHorarios () {
+    @FXML fun searchSchedule () {
         val visitDao = VisitDAO()
         val selectedDate : LocalDate? = dpDate.value
 
         if (selectedDate != null) {
-            val visitResult = visitDao.getUnavailableVisits(idProperty,Date.valueOf(selectedDate))
+            val visitResult = visitDao.getUnavailableVisits(propertyId,Date.valueOf(selectedDate))
             when (visitResult) {
                 is VisitResult.FoundList -> showSchedule(visitResult.visits)
-                is VisitResult.DBError -> showAlert("Error en la conexión con la base de datos")
-                else -> showAlert("Algo salió mal. Contacte a un técnico")
+                is VisitResult.DBError -> showAlert(visitResult.message,AlertType.ERROR)
+                is VisitResult.Failure -> showAlert(visitResult.message,AlertType.ERROR)
+                else -> showAlert("Algo salió mal. Contacte a un técnico",AlertType.ERROR)
             }
         }
-        else
-            showAlert("Debe seleccionar una fecha")
+        else showAlert("Debe seleccionar una fecha",AlertType.WARNING)
     }
 
     private fun showSchedule (unavailableVisits : List<Visit>) {
         val availableSchedule = mutableListOf("08:00:00","09:00:00","10:00:00","11:00:00","12:00:00","13:00:00","14:00:00","15:00:00","16:00:00")
 
         for (visit in unavailableVisits) {
-            val date = visit.date.toString()
-            availableSchedule.remove(date)
+            val time = visit.time.toString()
+            availableSchedule.remove(time)
         }
 
         var column = 0
         var row = 0
+        gpSchedule.children.clear()
 
         for (time in availableSchedule) {
             val fxmlLoader = FXMLLoader(javaClass.getResource("/FXML/ScheduleVisitItem.fxml"))
 
             try {
                 val panetime: BorderPane = fxmlLoader.load()
+                val scheduleVisitItemController = fxmlLoader.getController<ScheduleVisitItemController>()
+                scheduleVisitItemController.lbTime.text = time
+                scheduleVisitItemController.propertyId = propertyId
+                scheduleVisitItemController.clientId = clientId
+                scheduleVisitItemController.date = Date.valueOf(dpDate.value)
+                scheduleVisitItemController.parentController = this
                 gpSchedule.add(panetime,column,row)
             }
             catch (error: IOException) {
-                showAlert("Archivos corrompidos. Intentelo de nuevo más tarde")
+                showAlert("Archivos corrompidos. Intentelo de nuevo más tarde",AlertType.ERROR)
                 break
             }
 
-            if (row == 2) {
-                column++
-                row = 0
-            } else row++
+            if (column == 2) {
+                row++
+                column = 0
+            } else column++
         }
     }
 
-    private fun showAlert (message : String) {
-        val alert = Alert(Alert.AlertType.ERROR)
+    private fun showAlert (message : String, type : AlertType) {
+        val alert = Alert(type)
         alert.contentText = message
         alert.show()
     }
-
-
-
 }
